02f9d5d4663367d09e3db3050bf964ab83c5a281
lpm0073 <lpm0073@gmail.com>
2024-02-24 16:23:28 -0600

    chore: scaffold dashboard django temlates

--- smarter/smarter/apps/web_platform/views/account.py
+++ smarter/smarter/apps/web_platform/views/account.py
@@ -12,28 +12,28 @@ logger = logging.getLogger(__name__)
 @login_required
 def account(request):
     logger.info("Account view")
-    return render(request, "dashboard-default.html")
+    return render(request, "account/account.html")
 
 
 @login_required
 def account_organization(request):
     logger.info("Account-organization view")
-    return render(request, "dashboard-default.html")
+    return render(request, "account/organization.html")
 
 
 @login_required
 def account_team(request):
     logger.info("Account-team view")
-    return render(request, "dashboard-default.html")
+    return render(request, "account/team.html")
 
 
 @login_required
 def account_limits(request):
     logger.info("Account-limits view")
-    return render(request, "dashboard-default.html")
+    return render(request, "account/limits.html")
 
 
 @login_required
 def account_profile(request):
     logger.info("Account-profile view")
-    return render(request, "dashboard-default.html")
+    return render(request, "account/profile.html")
--- smarter/smarter/apps/web_platform/views/dashboard.py
+++ smarter/smarter/apps/web_platform/views/dashboard.py
@@ -12,40 +12,40 @@ logger = logging.getLogger(__name__)
 @login_required
 def dashboard(request):
     logger.info("Dashboard view")
-    return render(request, "dashboard-default.html")
+    return render(request, "dashboard/dashboard.html")
 
 
 @login_required
 def api_keys(request):
     logger.info("API keys view")
-    return render(request, "dashboard-default.html")
+    return render(request, "dashboard/api-keys.html")
 
 
 @login_required
 def plugins(request):
     logger.info("Plugins view")
-    return render(request, "dashboard-default.html")
+    return render(request, "dashboard/plugins.html")
 
 
 @login_required
 def usage(request):
     logger.info("Usage view")
-    return render(request, "dashboard-default.html")
+    return render(request, "dashboard/usage.html")
 
 
 @login_required
 def documentation(request):
     logger.info("Documentation view")
-    return render(request, "dashboard-default.html")
+    return render(request, "dashboard/documentation.html")
 
 
 @login_required
 def platform_help(request):
     logger.info("Help view")
-    return render(request, "dashboard-default.html")
+    return render(request, "dashboard/help.html")
 
 
 @login_required
 def notifications(request):
     logger.info("Notifications view")
-    return render(request, "dashboard-default.html")
+    return render(request, "dashboard/notifications.html")
