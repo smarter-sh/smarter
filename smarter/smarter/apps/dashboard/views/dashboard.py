5cdd0609a6eef3f3f61cab81513efe65e94f74d1
lpm0073 <lpm0073@gmail.com>
2024-03-01 16:46:58 -0600

    chore: cache page return

--- smarter/smarter/apps/dashboard/views/dashboard.py
+++ smarter/smarter/apps/dashboard/views/dashboard.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=W0613
 """Django views"""
 import logging
 
@@ -7,7 +6,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
 
-from smarter.view_helpers import clean_http_response
+from smarter.view_helpers import cached_clean_http_response
 
 
 logger = logging.getLogger(__name__)
@@ -17,46 +16,46 @@ logger = logging.getLogger(__name__)
 @cache_control(max_age=settings.SMARTER_CACHE_EXPIRATION)
 def dashboard(request):
     logger.info("Dashboard view")
-    return clean_http_response(template_path="dashboard/dashboard.html")
+    return cached_clean_http_response(request=request, template_path="dashboard/dashboard.html")
 
 
 @login_required
 @cache_control(max_age=settings.SMARTER_CACHE_EXPIRATION)
 def api_keys(request):
     logger.info("API keys view")
-    return clean_http_response(template_path="dashboard/api-keys.html")
+    return cached_clean_http_response(request=request, template_path="dashboard/api-keys.html")
 
 
 @login_required
 @cache_control(max_age=settings.SMARTER_CACHE_EXPIRATION)
 def plugins(request):
     logger.info("Plugins view")
-    return clean_http_response(template_path="dashboard/plugins.html")
+    return cached_clean_http_response(request=request, template_path="dashboard/plugins.html")
 
 
 @login_required
 @cache_control(max_age=settings.SMARTER_CACHE_EXPIRATION)
 def usage(request):
     logger.info("Usage view")
-    return clean_http_response(template_path="dashboard/usage.html")
+    return cached_clean_http_response(request=request, template_path="dashboard/usage.html")
 
 
 @login_required
 @cache_control(max_age=settings.SMARTER_CACHE_EXPIRATION)
 def documentation(request):
     logger.info("Documentation view")
-    return clean_http_response(template_path="dashboard/documentation.html")
+    return cached_clean_http_response(request=request, template_path="dashboard/documentation.html")
 
 
 @login_required
 @cache_control(max_age=settings.SMARTER_CACHE_EXPIRATION)
 def platform_help(request):
     logger.info("Help view")
-    return clean_http_response(template_path="dashboard/help.html")
+    return cached_clean_http_response(request=request, template_path="dashboard/help.html")
 
 
 @login_required
 @cache_control(max_age=settings.SMARTER_CACHE_EXPIRATION)
 def notifications(request):
     logger.info("Notifications view")
-    return clean_http_response(template_path="dashboard/notifications.html")
+    return cached_clean_http_response(request=request, template_path="dashboard/notifications.html")
--- smarter/smarter/view_helpers.py
+++ smarter/smarter/view_helpers.py
@@ -3,8 +3,10 @@
 import re
 
 from django import template
+from django.conf import settings
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.cache import cache_page
 
 
 register = template.Library()
@@ -15,7 +17,10 @@ def remove_comments(html):
     return re.sub(r"<!--.*?-->", "", html)
 
 
-def clean_http_response(template_path):
+# pylint: disable=W0613
+@cache_page(settings.SMARTER_CACHE_EXPIRATION)
+def cached_clean_http_response(request, template_path):
+    """Render a template and return an HttpResponse with comments removed."""
     loaded_template = loader.get_template(template_path)
     html = loaded_template.render()
     html_no_comments = remove_comments(html)
