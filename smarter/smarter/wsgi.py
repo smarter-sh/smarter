27f106c8c760cbb9cfbb702dc8556c11cc6e30fc
lpm0073 <lpm0073@gmail.com>
2024-03-01 19:28:23 -0600

    refactor: ensure that we have a base path

--- smarter/smarter/wsgi.py
+++ smarter/smarter/wsgi.py
@@ -3,14 +3,12 @@
 # wsgi.py
 import logging
 import os
-from pathlib import Path
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 from whitenoise import WhiteNoise
 
-
-BASE_DIR = Path(__file__).resolve().parent
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smarter.settings.local")
 
@@ -19,16 +17,13 @@ environment = os.getenv("ENVIRONMENT")
 if environment == "prod":
     os.environ["DJANGO_SETTINGS_MODULE"] = "smarter.settings.production"
 
-static_root = BASE_DIR / "staticfiles"
 application = get_wsgi_application()
-application = WhiteNoise(application, root=static_root)
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
 
-# Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-logger.info("WSGI config for smarter project.")
+logger.info("WSGI config for smarter.")
 logger.info("Environment: %s", environment)
 logger.info("WSGI application: %s", application)
-logger.info("BASE_DIR: %s", BASE_DIR)
 logger.info("DJANGO_SETTINGS_MODULE: %s", os.getenv("DJANGO_SETTINGS_MODULE"))
+logger.info("static_root: %s", settings.STATIC_ROOT)
