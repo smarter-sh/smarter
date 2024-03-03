dc85ca8a64908bb9eedc3b58fab6e3c0e9259968
lpm0073 <lpm0073@gmail.com>
2024-03-03 12:09:38 -0600

    chore: add minification

--- smarter/smarter/view_helpers.py
+++ smarter/smarter/view_helpers.py
@@ -10,6 +10,7 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.cache import cache_control, cache_page
+from htmlmin.main import minify
 from knox.auth import TokenAuthentication
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.generics import ListAPIView
@@ -36,7 +37,10 @@ class SmarterAPIListView(ListAPIView):
 
 
 class SmarterWebView(View):
-    """Base view for smarter web views."""
+    """
+    Base view for smarter web views.
+    Includes helpers for rendering, minifying and stripping out developer comments.
+    """
 
     template_path: str = ""
 
@@ -45,13 +49,18 @@ class SmarterWebView(View):
         """Remove HTML comments from an html string."""
         return re.sub(r"<!--.*?-->", "", html)
 
+    def minify_html(self, html):
+        """Minify an html string."""
+        return minify(html, remove_empty_space=True)
+
     # pylint: disable=W0613
     def clean_http_response(self, request, template_path):
         """Render a template and return an HttpResponse with comments removed."""
         response = render(request=request, template_name=template_path, context={})
         html = response.content.decode(response.charset)
-        html_no_comments = self.remove_comments(html)
-        return HttpResponse(html_no_comments)
+        html_no_comments = self.remove_comments(html=html)
+        minified_html = self.minify_html(html=html_no_comments)
+        return HttpResponse(minified_html)
 
     def get(self, request):
         return self.clean_http_response(request, template_path=self.template_path)
