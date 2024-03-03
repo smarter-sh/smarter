942f456a0d6c0648fbcff0895b879475ab4147b2
lpm0073 <lpm0073@gmail.com>
2024-03-03 17:06:07 -0600

    refactor: convert remaining functional views to class-based views

--- smarter/smarter/apps/api/v0/views.py
+++ smarter/smarter/apps/api/v0/views.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=W0613
 """Django REST framework views for the API admin app."""
-from django.shortcuts import render
-
 from smarter.view_helpers import SmarterAPIAdminView
-
-
-def custom_api_root(request):
-    return render(request, "rest_framework/root_page_template.html")
 
 
 class CustomAPIView(SmarterAPIAdminView):
