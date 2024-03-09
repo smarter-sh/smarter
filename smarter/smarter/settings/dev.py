edb9794629d0c000fc84a2207738547ec28bc391
lpm0073 <lpm0073@gmail.com>
2024-03-09 07:37:03 -0600

    chore: set SMTP_SENDER in each environment

--- smarter/smarter/settings/dev.py
+++ smarter/smarter/settings/dev.py
@@ -5,8 +5,13 @@
 from .base_aws import *
 
 
-ALLOWED_HOSTS = ["dev.platform.smarter.sh"]
+ENVIRONMENT_DOMAIN = "dev.platform.smarter.sh"
+
+ALLOWED_HOSTS = [ENVIRONMENT_DOMAIN]
 
 # (4_0.E001) As of Django 4.0, the values in the CSRF_TRUSTED_ORIGINS setting must start with a scheme
 # (usually http:// or https://) but found platform.smarter.sh. See the release notes for details.
 CSRF_TRUSTED_ORIGINS = [f"http://{host}" for host in ALLOWED_HOSTS] + [f"https://{host}" for host in ALLOWED_HOSTS]
+
+SMTP_SENDER = os.environ.get("SMTP_SENDER", ENVIRONMENT_DOMAIN)
+SMTP_FROM_EMAIL = os.environ.get("SMTP_FROM_EMAIL", "no-reply@" + SMTP_SENDER)
