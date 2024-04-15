adb050d7a7e14182053995d43a3bf734496e21fa
lpm0073 <lpm0073@gmail.com>
2024-04-15 16:51:06 -0600

    docs: improve the doc string

--- smarter/smarter/apps/chatbot/middleware.py
+++ smarter/smarter/apps/chatbot/middleware.py
@@ -1,7 +1,7 @@
 """This module is used to suppress DisallowedHost exception and return HttpResponseBadRequest instead."""
 
 from django.conf import settings
-from django.core.exceptions import DisallowedHost
+from django.http import HttpResponseBadRequest
 from django.middleware.security import SecurityMiddleware as DjangoSecurityMiddleware
 
 from .models import ChatBot
@@ -9,17 +9,27 @@ from .models import ChatBot
 
 class SecurityMiddleware(DjangoSecurityMiddleware):
     """
-    Override Django's SecurityMiddleware to suppress DisallowedHost exception
-    for ChatBot API domains.
+    Override Django's SecurityMiddleware to create our own implementation
+    of ALLOWED_HOSTS, referred to as SMARTER_ALLOWED_HOSTS.
+
+    We not only need to evaluate the traditional list of ALLOWED_HOSTS, but
+    also need to check if the host is a domain for a deployed ChatBot. If the
+    host is a domain for a deployed ChatBot, we should allow the request to
+    pass through.
+
+    This middleware is also used to suppress the stock DisallowedHost exception
+    in favor of our own HttpResponseBadRequest response, which is a non-logged
+    response that is more user-friendly.
+
     """
 
     def process_request(self, request):
 
         host = request.get_host()
-        if host in settings.ALLOWED_HOSTS:
+        if host in settings.SMARTER_ALLOWED_HOSTS:
             return None
 
         if ChatBot.get_by_url(host) is not None:
             return None
 
-        raise DisallowedHost
+        return HttpResponseBadRequest("Bad Request (400) - Invalid Hostname")
