43a60ef05d2e3562e18a43c47436c3c00f6ca89f
lpm0073 <lpm0073@gmail.com>
2024-04-17 22:43:36 -0600

    test: add http_method_names

--- smarter/smarter/apps/chatbot/deployments/base.py
+++ smarter/smarter/apps/chatbot/deployments/base.py
@@ -7,9 +7,8 @@ from http import HTTPStatus
 from typing import List
 
 from cachetools import TTLCache, cached
-from django.http import HttpResponseForbidden, HttpResponseNotFound, JsonResponse
+from django.http import JsonResponse
 from knox.auth import TokenAuthentication
-from rest_framework.views import APIView
 
 from smarter.apps.account.models import Account, UserProfile
 from smarter.apps.account.utils import account_admin_user
@@ -25,14 +24,16 @@ from smarter.common.conf import settings as smarter_settings
 from smarter.common.exceptions import SmarterBusinessRuleViolation
 from smarter.lib.django.user import UserType
 from smarter.lib.django.validators import SmarterValidator
-from smarter.lib.django.view_helpers import SmarterNeverCachedWebView
+from smarter.lib.drf.view_helpers import SmarterUnauthenticatedAPIView
 
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 cache = TTLCache(ttl=600, maxsize=1000)
 
 
-class ChatBotApiBaseViewSet(SmarterNeverCachedWebView):
+class ChatBotApiBaseViewSet(SmarterUnauthenticatedAPIView):
     """
     Base viewset for all ChatBot APIs. Handles
     - api key authentication
@@ -40,6 +41,7 @@ class ChatBotApiBaseViewSet(SmarterNeverCachedWebView):
     - dispatching.
     """
 
+    http_method_names = ["get", "post"]
     helper: ChatBotApiUrlHelper = None
     account: Account = None
     user: UserType = None
@@ -101,8 +103,10 @@ class ChatBotApiBaseViewSet(SmarterNeverCachedWebView):
     def dispatch(self, request, *args, **kwargs):
         url = request.build_absolute_uri()
         url = SmarterValidator.urlify(url)
-        logger.debug("ChatBotApiBaseViewSet.dispatch: url=%s", url)
-        logger.debug("ChatBotApiBaseViewSet.dispatch: user=%s", request.user)
+        logger.info("ChatBotApiBaseViewSet.dispatch(): url=%s", url)
+        logger.info("ChatBotApiBaseViewSet.dispatch(): user=%s", request.user)
+        logger.info("ChatBotApiBaseViewSet.dispatch(): method=%s", request.method)  # Log the method
+
         self.user = request.user
         if request.user.is_authenticated:
             self.helper = self.get_cached_helper(url=url, user=request.user)
@@ -110,27 +114,27 @@ class ChatBotApiBaseViewSet(SmarterNeverCachedWebView):
             self.helper = self.get_cached_helper(url=url)
 
         if not self.helper.is_valid:
-            return HttpResponseNotFound(
-                content={
-                    "message": "Not Found. Please provide a valid ChatBot URL.",
-                    "account": self.helper.account,
-                    "chatbot": self.helper.chatbot,
-                    "user": self.helper.user,
-                    "url": self.helper.url,
-                }
-            )
+            data = {
+                "message": "Not Found. Please provide a valid ChatBot URL.",
+                "account": self.helper.account.account_number if self.helper.account else None,
+                "chatbot": self.helper.chatbot,
+                "user": self.helper.user.username if self.helper.user else None,
+                "url": self.helper.url,
+            }
+            return JsonResponse(data=data, status=HTTPStatus.BAD_REQUEST)
         self.chatbot = self.helper.chatbot
         if self.helper.is_authentication_required and not self.smarter_api_authenticate(request):
-            return HttpResponseForbidden(content={"message": "Forbidden. Please provide a valid API key."})
+            data = {"message": "Forbidden. Please provide a valid API key."}
+            return JsonResponse(data=data, status=HTTPStatus.FORBIDDEN)
         self.account = self.helper.account
         self.plugins = ChatBotPlugin().plugins(chatbot=self.chatbot)
 
         self.user = self.user or account_admin_user(self.account)
 
-        logger.debug("ChatBotApiBaseViewSet.dispatch: done. account=%s", self.account)
-        logger.debug("ChatBotApiBaseViewSet.dispatch: done. chatbot=%s", self.chatbot)
-        logger.debug("ChatBotApiBaseViewSet.dispatch: done. user=%s", self.user)
-        logger.debug("ChatBotApiBaseViewSet.dispatch: done. plugins=%s", self.plugins)
+        logger.debug("ChatBotApiBaseViewSet.dispatch(): account=%s", self.account)
+        logger.debug("ChatBotApiBaseViewSet.dispatch(): chatbot=%s", self.chatbot)
+        logger.debug("ChatBotApiBaseViewSet.dispatch(): user=%s", self.user)
+        logger.debug("ChatBotApiBaseViewSet.dispatch(): plugins=%s", self.plugins)
 
         chatbot_called.send(sender=self.__class__, chatbot=self.chatbot, request=request, args=args, kwargs=kwargs)
         return super().dispatch(request, *args, **kwargs)
@@ -148,4 +152,4 @@ class ChatBotApiBaseViewSet(SmarterNeverCachedWebView):
             "account": self.account.account_number if self.account else None,
             "user": self.user.username if self.user else None,
         }
-        return JsonResponse(data=retval, status=HTTPStatus.METHOD_NOT_ALLOWED)
+        return JsonResponse(data=retval, status=HTTPStatus.OK)
