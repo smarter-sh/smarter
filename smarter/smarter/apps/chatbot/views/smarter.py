5b1081ed51005586a82a67aa6414c1adc452e23b
lpm0073 <lpm0073@gmail.com>
2024-04-26 17:44:20 -0600

    refactor: remove rendundant class variables

--- smarter/smarter/apps/chatbot/views/smarter.py
+++ smarter/smarter/apps/chatbot/views/smarter.py
@@ -8,31 +8,13 @@ from http import HTTPStatus
 
 import waffle
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
 
-from smarter.apps.account.api.v0.serializers import AccountSerializer
-from smarter.apps.chat.api.v0.serializers import (
-    ChatHistorySerializer,
-    ChatPluginUsageSerializer,
-    ChatSerializer,
-    ChatToolCallSerializer,
-)
-from smarter.apps.chat.models import (
-    Chat,
-    ChatHelper,
-    ChatHistory,
-    ChatPluginUsage,
-    ChatToolCall,
-)
+from smarter.apps.chat.models import ChatHelper
 from smarter.apps.chat.providers.smarter import handler
-from smarter.apps.chatbot.api.v0.serializers import ChatBotSerializer
 from smarter.lib.django.validators import SmarterValidator
 
 from .base import ChatBotApiBaseViewSet
 
-
-MAX_RETURNED_HISTORY = 25
-CACHE_EXPIRATION = 60 * 30  # 5 minutes
 
 logger = logging.getLogger(__name__)
 
@@ -42,8 +24,6 @@ class SmarterChatBotApiView(ChatBotApiBaseViewSet):
 
     data: dict = {}
     chat_helper: ChatHelper = None
-    chat: Chat = None
-    session_key: str = None
 
     """
     top-level viewset for customer-deployed Plugin-based Chat APIs.
@@ -67,47 +47,15 @@ class SmarterChatBotApiView(ChatBotApiBaseViewSet):
         # json dict that includes, among other pertinent info, this session_key
         # which uniquely identifies the device and the individual chatbot session
         # for the device.
-        self.session_key = self.data.get("session_key")
-        SmarterValidator.validate_session_key(self.session_key)
-
-        self.chat_helper = ChatHelper(session_key=self.session_key, request=request)
-        self.chat = self.chat_helper.chat
+        session_key = self.data.get("session_key")
+        if session_key:
+            SmarterValidator.validate_session_key(session_key)
+        self.chat_helper = ChatHelper(session_key=session_key, request=request)
 
         if waffle.switch_is_active("chatbot_api_view_logging"):
-            logger.info("%s initialized with chat object %s", self.formatted_class_name, self.chat)
+            logger.info("%s initialized with chat object %s", self.formatted_class_name, self.chat_helper.chat)
 
         return super().dispatch(request, *args, **kwargs)
-
-    # pylint: disable=W0613
-    def get(self, request, *args, **kwargs):
-        if waffle.switch_is_active("chatbot_api_view_logging"):
-            logger.info("%s.get() - %s", self.formatted_class_name, self.chat)
-
-        chat = get_object_or_404(Chat, session_key=self.session_key)
-        chat_history = ChatHistory.objects.filter(chat=chat).order_by("-pk")[:MAX_RETURNED_HISTORY]
-        chat_tool_calls = ChatToolCall.objects.filter(chat=chat)
-        chat_plugin_usage = ChatPluginUsage.objects.filter(chat=chat)
-
-        data = {
-            "session_key": self.session_key,
-            "account": AccountSerializer(self.account).data if self.account else None,
-            "chatbot": ChatBotSerializer(self.chatbot).data if self.chatbot else None,
-            "chat": ChatSerializer(self.chat).data if self.chat else None,
-            "history": (
-                [ChatHistorySerializer(instance).data for instance in chat_history] if chat_history.exists() else None
-            ),
-            "tool_calls": (
-                [ChatToolCallSerializer(instance).data for instance in chat_tool_calls]
-                if chat_tool_calls.exists()
-                else None
-            ),
-            "plugin_usage": (
-                [ChatPluginUsageSerializer(instance).data for instance in chat_plugin_usage]
-                if chat_plugin_usage.exists()
-                else None
-            ),
-        }
-        return JsonResponse(data=data, safe=False, status=HTTPStatus.OK)
 
     # pylint: disable=W0613
     def post(self, request, *args, **kwargs):
@@ -139,12 +87,12 @@ class SmarterChatBotApiView(ChatBotApiBaseViewSet):
             logger.info("%s.post() - data=%s", self.formatted_class_name, self.data)
             logger.info("%s.post() - account: %s", self.formatted_class_name, self.account)
             logger.info("%s.post() - user: %s", self.formatted_class_name, self.user)
-            logger.info("%s.post() - chat: %s", self.formatted_class_name, self.chat)
+            logger.info("%s.post() - chat: %s", self.formatted_class_name, self.chat_helper.chat)
             logger.info("%s.post() - chatbot: %s", self.formatted_class_name, self.chatbot)
             logger.info("%s.post() - plugins: %s", self.formatted_class_name, self.plugins)
 
         response = handler(
-            chat=self.chat,
+            chat=self.chat_helper.chat,
             data=self.data,
             plugins=self.plugins,
             user=self.user,
