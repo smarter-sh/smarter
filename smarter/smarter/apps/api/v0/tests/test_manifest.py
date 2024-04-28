c627150e42c4e8a604bca1e682d68fafb23544d3
lpm0073 <lpm0073@gmail.com>
2024-04-28 12:34:46 -0600

    test: add unit test for Pydantic model

---
+++ smarter/smarter/apps/api/v0/tests/test_manifest.py
@@ -0,0 +1,29 @@
+# pylint: disable=wrong-import-position
+"""Test TestSAM."""
+
+import os
+import unittest
+
+from smarter.apps.api.v0.manifests.handler import SAMHandler
+from smarter.apps.api.v0.manifests.version import SMARTER_API_VERSION
+from smarter.common.const import PYTHON_ROOT
+
+
+class TestSAM(unittest.TestCase):
+    """Test TestSAM"""
+
+    def setUp(self):
+        """Set up test fixtures."""
+        self.path = os.path.join(PYTHON_ROOT, "smarter", "apps", "api", "v0", "tests", "data")
+        self.good_manifest_path = os.path.join(self.path, "good-manifest.yaml")
+        self.invalid_file_format = os.path.join(self.path, "invalid-file-format.yaml")
+
+    def test_valid_manifest(self):
+        """Test valid file path and that we can instantiate with errors"""
+
+        handler = SAMHandler(file_path=self.good_manifest_path)
+        manifest = handler.manifest
+        self.assertEqual(manifest.apiVersion, SMARTER_API_VERSION)
+        self.assertEqual(manifest.kind, "Plugin")
+        self.assertEqual(manifest.metadata.name, "ExampleConfiguration")
+        self.assertEqual(manifest.metadata.version, "0.2.0")
