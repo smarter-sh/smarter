c108b02acc2c2d54ae933f2cb1d4c6db6f0a784d
lpm0073 <lpm0073@gmail.com>
2024-04-29 12:20:54 -0600

    chore: swap string literals for enumerated values

--- smarter/smarter/apps/plugin/api/v0/manifests/models/plugin.py
+++ smarter/smarter/apps/plugin/api/v0/manifests/models/plugin.py
@@ -10,6 +10,7 @@ from smarter.apps.api.v0.manifests.models import SAM
 # Plugin
 from smarter.apps.plugin.api.v0.manifests.enum import SAMPluginMetadataClassValues
 
+from ..enum import SAMPluginMetadataClassValues
 from .metadata import SAMPluginMetadata
 from .spec import SAMPluginSpec
 from .status import SAMPluginStatus
@@ -29,12 +30,18 @@ class SAMPlugin(SAM):
         """Plugin-level business rule validations"""
 
         if self.metadata.plugin_class == SAMPluginMetadataClassValues.STATIC and not self.spec.data.static_data:
-            raise SAMValidationError("spec.data.staticData is required when Plugin.class is 'static'")
+            raise SAMValidationError(
+                f"spec.data.staticData is required when Plugin.class is '{SAMPluginMetadataClassValues.STATIC.value}'"
+            )
 
         if self.metadata.plugin_class == SAMPluginMetadataClassValues.SQL and not self.spec.data.sql_data:
-            raise SAMValidationError("spec.data.sqlData is required when Plugin.class is 'sql'")
+            raise SAMValidationError(
+                f"spec.data.sqlData is required when Plugin.class is '{SAMPluginMetadataClassValues.SQL.value}'"
+            )
 
         if self.metadata.plugin_class == SAMPluginMetadataClassValues.API and not self.spec.data.api_data:
-            raise SAMValidationError("spec.data.apiData is required when Plugin.class is 'api'")
+            raise SAMValidationError(
+                f"spec.data.apiData is required when Plugin.class is '{SAMPluginMetadataClassValues.API.value}'"
+            )
 
         return self
