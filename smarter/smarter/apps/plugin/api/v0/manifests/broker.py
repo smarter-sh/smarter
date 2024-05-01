33d4905aeb711c9e87b6ba3dc9f4413fcfeedf81
lpm0073 <lpm0073@gmail.com>
2024-04-30 20:30:14 -0600

    refactor: make all initializations lazy

--- smarter/smarter/apps/plugin/api/v0/manifests/broker.py
+++ smarter/smarter/apps/plugin/api/v0/manifests/broker.py
@@ -1,15 +1,13 @@
 """Smarter API Plugin Manifest handler"""
 
-import logging
-
 from smarter.apps.account.models import Account, UserProfile
 from smarter.apps.account.utils import account_admin_user
 from smarter.apps.api.v0.manifests.broker import SAMBroker
 from smarter.apps.plugin.api.v0.manifests.models.plugin import SAMPlugin
+from smarter.apps.plugin.plugin.base import PluginBase
+from smarter.apps.plugin.plugin.controller import PluginController
 from smarter.lib.django.user import UserType
-
-
-logger = logging.getLogger(__name__)
+from smarter.lib.django.validators import SmarterValidator
 
 
 class SAMPluginBroker(SAMBroker):
@@ -27,30 +25,41 @@ class SAMPluginBroker(SAMBroker):
     _account: Account = None
     _user: UserType = None
     _user_profile: UserProfile = None
+    _plugin: PluginBase = None
 
     def __init__(
         self,
-        account_number: str = None,
+        account_number: str,
         manifest: str = None,
         file_path: str = None,
         url: str = None,
     ):
-        # 1.) Load, validate and parse the manifest. The parent will initialize
-        # the generic manifest loader class, SAMLoader(), which can then be used to
-        # provide initialization data to any kind of manifest model. the loader
-        # also performs cursory high-level validation of the manifest, sufficient
-        # to ensure that the manifest is a valid yaml file and that it contains
-        # the required top-level keys.
+        """
+        Load, validate and parse the manifest. The parent will initialize
+        the generic manifest loader class, SAMLoader(), which can then be used to
+        provide initialization data to any kind of manifest model. the loader
+        also performs cursory high-level validation of the manifest, sufficient
+        to ensure that the manifest is a valid yaml file and that it contains
+        the required top-level keys.
+        """
+        SmarterValidator.validate_account_number(account_number)
+        self._account = Account.objects.get(account_number=account_number)
         super().__init__(account_number=account_number, manifest=manifest, file_path=file_path, url=url)
 
-        # 2.) Initialize the Plugin manifest model. SAMPlugin() is a Pydantic model
-        # that is used to represent the Smarter API Plugin manifest. The Pydantic
-        # model is initialized with the data from the manifest loader, which is
-        # generally passed to the model constructor as **data. However, this top-level
-        # manifest model has to be explicitly initialized, whereas its child models
-        # are automatically cascade-initialized by the Pydantic model, implicitly
-        # passing **data to each child's constructor.
-
+    # override the base abstract manifest model with the Plugin model
+    @property
+    def manifest(self) -> SAMPlugin:
+        """
+        SAMPlugin() is a Pydantic model
+        that is used to represent the Smarter API Plugin manifest. The Pydantic
+        model is initialized with the data from the manifest loader, which is
+        generally passed to the model constructor as **data. However, this top-level
+        manifest model has to be explicitly initialized, whereas its child models
+        are automatically cascade-initialized by the Pydantic model, implicitly
+        passing **data to each child's constructor.
+        """
+        if self._manifest:
+            return self._manifest
         self._manifest = SAMPlugin(
             apiVersion=self.loader.manifest_api_version,
             kind=self.loader.manifest_kind,
@@ -58,14 +67,6 @@ class SAMPluginBroker(SAMBroker):
             spec=self.loader.manifest_spec,
             status=self.loader.manifest_status,
         )
-
-        self._account = Account.objects.get(account_number=account_number)
-        self._user = account_admin_user(account_number)
-        self._user_profile = UserProfile.objects.get(user=self.user, account=self.account)
-
-    # override the base abstract manifest model with the Plugin model
-    @property
-    def manifest(self) -> SAMPlugin:
         return self._manifest
 
     @property
@@ -74,8 +75,26 @@ class SAMPluginBroker(SAMBroker):
 
     @property
     def user(self) -> UserType:
+        if self._user:
+            return self._user
+        self._user = account_admin_user(self.manifest.metadata.account)
         return self._user
 
     @property
     def user_profile(self) -> UserProfile:
+        if self._user_profile:
+            return self._user_profile
+        self._user_profile = UserProfile.objects.get(user=self.user, account=self.account)
         return self._user_profile
+
+    @property
+    def plugin(self) -> PluginBase:
+        """
+        PluginController() is a helper class to map the manifest model
+        metadata.pluginClass to an instance of the the correct plugin class.
+        """
+        if self._plugin:
+            return self._plugin
+        controller = PluginController(self.manifest)
+        self._plugin = controller.plugin
+        return self._plugin
