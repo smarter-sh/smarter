8941507b316d8e48d120bfca150452da3a066381
lpm0073 <lpm0073@gmail.com>
2024-05-10 13:39:28 -0600

    chore: link plugin kind descriptor

--- smarter/smarter/apps/api/v1/manifests/enum.py
+++ smarter/smarter/apps/api/v1/manifests/enum.py
@@ -1,12 +1,13 @@
 """Smarter API V0 Manifests Enumerations."""
 
+from smarter.apps.plugin.manifest.const import MANIFEST_KIND as PLUGIN_KIND
 from smarter.lib.manifest.enum import SmarterEnumAbstract
 
 
 class SAMKinds(SmarterEnumAbstract):
     """Smarter manifest kinds enumeration."""
 
-    PLUGIN = "Plugin"
+    PLUGIN = PLUGIN_KIND
     ACCOUNT = "Account"
     USER = "User"
     CHAT = "Chat"
