a7e0b70658d2eb1819a7e7a4c92a6762538afec9
lpm0073 <lpm0073@gmail.com>
2024-05-10 08:18:08 -0600

    chore: make command runnable in any environment

--- smarter/smarter/apps/api/management/commands/verify_api_v1_cli_endpoints.py
+++ smarter/smarter/apps/api/management/commands/verify_api_v1_cli_endpoints.py
@@ -11,32 +11,44 @@ from smarter.apps.account.models import Account
 from smarter.apps.account.utils import account_admin_user
 from smarter.common.conf import settings as smarter_settings
 from smarter.common.const import SMARTER_ACCOUNT_NUMBER, SmarterEnvironments
+from smarter.lib.django.user import User
 
 
 class Command(BaseCommand):
-    """utility for running api/v1 cli endpoints to verify that they work."""
-
-    account = Account.get_account_by_number(SMARTER_ACCOUNT_NUMBER)
-    user = account_admin_user(account=account)
+    """
+    Utility for running api/v1/cli/ endpoints to verify that they work.
+    This largely recreates the unit tests for the endpoints, albeit with
+    formatted screen output.
+    """
 
     def add_arguments(self, parser):
         """Add arguments to the command."""
+        parser.add_argument(
+            "account_number", type=str, nargs="?", default=f"{SMARTER_ACCOUNT_NUMBER}", help="a Smarter account number."
+        )
         parser.add_argument(
             "username", type=str, nargs="?", default="admin", help="A user associated with the Smarter account."
         )
 
     def handle(self, *args, **options):
+        username = options["username"]
+        account_number = options["account_number"]
+
+        account = Account.get_account_by_number(account_number)
+        user = account_admin_user(account=account)
+        if username != user.get_username():
+            user = User.objects.get(username=username)
 
         print("*" * 80)
         print("Running API CLI endpoint verifications.")
         print("Environment: ", smarter_settings.environment)
-        print("Account: ", SMARTER_ACCOUNT_NUMBER)
-        print("User: ", self.user.username)
+        print("Account: ", account_number)
+        print("User: ", user.username)
         print("*" * 80)
 
         def get_response(path):
             client = Client()
-            client.force_login(self.user)
+            client.force_login(user)
 
             if smarter_settings.environment in SmarterEnvironments.aws_environments:
                 response = client.post(path=path, HTTP_HOST=smarter_settings.environment_domain)
