79c1352175a3a0bfe79bdfb3a30a28a02d6c880b
lpm0073 <lpm0073@gmail.com>
2024-05-13 14:56:31 -0600

    test: fix up Account creation

--- smarter/smarter/apps/plugin/api/v1/tests/test_manifest.py
+++ smarter/smarter/apps/plugin/api/v1/tests/test_manifest.py
@@ -19,7 +19,15 @@ class TestSAM(unittest.TestCase):
         self.path = os.path.join(PYTHON_ROOT, "smarter", "apps", "api", "v1", "tests", "data")
         self.good_manifest_path = os.path.join(self.path, "good-manifest.yaml")
         self.invalid_file_format = os.path.join(self.path, "invalid-file-format.yaml")
-        self.account = Account.objects.create(name="Test Account")
+        self.account = Account.objects.create(
+            company_name="Test Company",
+            phone_number="1234567890",
+            address1="123 Test St",
+            address2="Apt 1",
+            city="Test City",
+            state="TX",
+            postal_code="12345",
+        )
 
     def test_valid_manifest(self):
         """Test valid file path and that we can instantiate with errors"""
