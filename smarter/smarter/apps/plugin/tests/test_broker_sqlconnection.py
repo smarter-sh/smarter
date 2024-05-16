c85a83c50077ad8b626a7078083de2c858df32bb
lpm0073 <lpm0073@gmail.com>
2024-05-16 16:53:04 -0600

    test: wrap up SAMPluginDataSqlConnection() unit tests

--- smarter/smarter/apps/plugin/tests/test_broker_sqlconnection.py
+++ smarter/smarter/apps/plugin/tests/test_broker_sqlconnection.py
@@ -11,6 +11,10 @@ from smarter.apps.account.tests.factories import admin_user_factory, admin_user_
 from smarter.apps.plugin.manifest.brokers.sql_connection import (
     SAMPluginDataSqlConnectionBroker,
 )
+from smarter.apps.plugin.manifest.models.sql_connection.model import (
+    SAMPluginDataSqlConnection,
+)
+from smarter.lib.manifest.loader import SAMLoader
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -43,17 +47,56 @@ class TestSAMPluginDataSqlConnectionBroker(unittest.TestCase):
         self.assertEqual(content["message"], "PluginDataSqlConnection testConnection applied successfully")
 
     def test_plugin_broker_describe(self):
-        """Test that the Broker can generate and return the manifest."""
+        """
+        Test that the Broker can generate and return a valid manifest.
+        - create a resource from a manifest
+        - describe the resource
+        - convert the description from json to yaml
+        - load the yaml description into a SAMLoader object
+        - create a pydantic model from the loader
+        - dump the pydantic model to a dictionary
+        """
+
+        def dict_is_contained_in(dict1, dict2):
+            for key, value in dict1.items():
+                if key not in dict2:
+                    print("key not in dict2: ", key)
+                    return False
+                if isinstance(value, dict):
+                    if not dict_is_contained_in(value, dict2[key]):
+                        print("dict not in dict2: ", value)
+                        return False
+                else:
+                    if dict2[key] != value:
+                        print("value not in dict2: ", value)
+                        return False
+            return True
+
         retval = self.broker.apply()
         self.assertEqual(retval.status_code, HTTPStatus.OK)
 
+        # generate the manifest
         retval = self.broker.describe()
         self.assertEqual(retval.status_code, HTTPStatus.OK)
+
+        # transform the json content to a yaml manifest
         content = json.loads(retval.content.decode())
         self.assertIsInstance(content, dict)
-        print(content)
-        # self.assertIn("message", content.keys())
-        # self.assertEqual(content["message"], "PluginDataSqlConnection testConnection deleted successfully")
+        content = content["data"]  # the manifest is loaded into the 'data' key
+        content.pop("status")  # status is read-only
+        manifest = yaml.dump(content)
+
+        # load the yaml manifest into a SAMLoader object
+        loader = SAMLoader(manifest=manifest)
+
+        # create a pydantic model from the loader
+        pydantic_model = SAMPluginDataSqlConnection(**loader.pydantic_model_dump())
+
+        # dump the pydantic model to a dictionary
+        round_trip_dict = pydantic_model.model_dump()
+
+        # assert that everything in content is in round_trip_dict
+        self.assertTrue(dict_is_contained_in(content, round_trip_dict))
 
     def test_plugin_broker_delete(self):
         """Test that the Broker can delete the object."""
@@ -66,3 +109,13 @@ class TestSAMPluginDataSqlConnectionBroker(unittest.TestCase):
         self.assertIsInstance(content, dict)
         self.assertIn("message", content.keys())
         self.assertEqual(content["message"], "PluginDataSqlConnection testConnection deleted successfully")
+
+    def test_plugin_broker_deploy(self):
+        """Test that the Broker does not implement a deploy() method."""
+
+        retval = self.broker.deploy()
+        self.assertEqual(retval.status_code, HTTPStatus.NOT_IMPLEMENTED)
+        content = json.loads(retval.content.decode())
+        self.assertIsInstance(content, dict)
+        self.assertIn("message", content.keys())
+        self.assertEqual(content["message"], "operation not implemented for PluginDataSqlConnection resources")
