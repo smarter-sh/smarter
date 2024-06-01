24cb25e84c6489c40c63409aaccf52c1a16eabf1
lpm0073 <lpm0073@gmail.com>
2024-06-01 13:58:40 -0600

    chore: only install django debut_toolbar when in DEBUG mode and not running unit tests

--- smarter/smarter/settings/local.py
+++ smarter/smarter/settings/local.py
@@ -33,7 +33,6 @@ INSTALLED_APPS += ["django_extensions"]
 if DEBUG and not "test" in sys.argv:
     INSTALLED_APPS += [
         "debug_toolbar",
-        "django_extensions",
     ]
 
     MIDDLEWARE += [
