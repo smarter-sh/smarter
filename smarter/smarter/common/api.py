b4aee89eb7119443d1e409766d24ba2cbfcfa1c7
lpm0073 <lpm0073@gmail.com>
2024-06-08 12:38:09 -0600

    chore: style docs template

--- smarter/smarter/common/api.py
+++ smarter/smarter/common/api.py
@@ -8,3 +8,7 @@ class SmarterApiVersions:
 
     V0 = f"{VERSION_PREFIX}/v0"
     V1 = f"{VERSION_PREFIX}/v1"
+
+    @classmethod
+    def all_values(cls):
+        return [value for name, value in vars(SmarterApiVersions).items() if not name.startswith("__")]
