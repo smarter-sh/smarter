c68a272a48263591d75732b1c09826e7ad350e23
lpm0073 <lpm0073@gmail.com>
2024-06-10 14:49:56 -0600

    refactor: create menu item 'CLI' and consolidate 'json schemas' and 'manifests'

--- smarter/smarter/apps/api/docs/urls.py
+++ smarter/smarter/apps/api/docs/urls.py
@@ -60,7 +60,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path("", DocsView.as_view(), name="docs-home"),
+    path("", DocsView.as_view(), name="api_docs_home"),
     path("sitemap", SiteMapView.as_view(), name="sitemap"),
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
