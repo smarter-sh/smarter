16258b02bd3c88f0ef9c365a2bbb3b0921d0a47c
lpm0073 <lpm0073@gmail.com>
2024-06-10 11:33:17 -0600

    chore: setup markdown rendering for files in /docs folder

--- smarter/smarter/apps/api/docs/urls.py
+++ smarter/smarter/apps/api/docs/urls.py
@@ -6,7 +6,18 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-from .views import DocsView, SiteMapView
+from .views import (
+    DeveloperDocsArchitectureView,
+    DeveloperDocsChatBotApiView,
+    DeveloperDocsCliView,
+    DeveloperDocsDjangoReactView,
+    DeveloperDocsGoodCodoingPracticeView,
+    DeveloperDocsOpenAIGettingStartedView,
+    DeveloperDocsSemanticVersioningView,
+    DeveloperDocsTwelveFactorView,
+    DocsView,
+    SiteMapView,
+)
 
 
 schema_view = get_schema_view(
@@ -29,4 +40,24 @@ urlpatterns = [
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc-ui"),
+    path("developer/12-factor/", DeveloperDocsTwelveFactorView.as_view(), name="developer-12-factor"),
+    path("developer/architecture/", DeveloperDocsArchitectureView.as_view(), name="developer-architecture"),
+    path("developer/chatbot-api/", DeveloperDocsChatBotApiView.as_view(), name="developer-chatbot-api"),
+    path("developer/cli/", DeveloperDocsCliView.as_view(), name="developer-cli"),
+    path("developer/django-react/", DeveloperDocsDjangoReactView.as_view(), name="developer-django-react"),
+    path(
+        "developer/good-coding-practice/",
+        DeveloperDocsGoodCodoingPracticeView.as_view(),
+        name="developer-good-coding-practice",
+    ),
+    path(
+        "developer/openai-getting-started/",
+        DeveloperDocsOpenAIGettingStartedView.as_view(),
+        name="developer-openai-getting-started",
+    ),
+    path(
+        "developer/semantic-versioning/",
+        DeveloperDocsSemanticVersioningView.as_view(),
+        name="developer-semantic-versioning",
+    ),
 ]
