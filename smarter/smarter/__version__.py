4bf2578bd916a2a709e346edf6a27f95304b22a2
GitHub Action <action@github.com>
2024-07-05 18:44:18 +0000

    chore: [gh] Sync __version__.py with latest GitHub release 0.5.3 [skip ci]

--- smarter/smarter/__version__.py
+++ smarter/smarter/__version__.py
@@ -7,3 +7,6 @@ __version__ = "0.5.1"
 # DO NOT EDIT.
 # Managed via automated CI/CD in .github/workflows/semanticVersionBump.yml.
 __version__ = "0.5.2"
+# DO NOT EDIT.
+# Managed via automated CI/CD in .github/workflows/semanticVersionBump.yml.
+__version__ = "0.5.3"
