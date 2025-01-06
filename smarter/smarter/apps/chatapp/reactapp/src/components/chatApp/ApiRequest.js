affbcc8dd4c4864ec1642b86f04677fcf1aab57f
lpm0073 <lpm0073@gmail.com>
2025-01-06 12:03:04 -0600

    fix: ensure that only one csrftoken cookie exists

--- smarter/smarter/apps/chatapp/reactapp/src/components/chatApp/ApiRequest.js
+++ smarter/smarter/apps/chatapp/reactapp/src/components/chatApp/ApiRequest.js
@@ -45,6 +45,12 @@ export async function processApiRequest(
   const csrftoken = getCookie("csrftoken");
   const sessionid = getCookie('sessionid');
 
+  // Ensure only one csrftoken is included in the Cookie header
+  const cookiesArray = document.cookie.split(';').filter(cookie => !cookie.trim().startsWith('csrftoken='));
+  cookiesArray.push(`csrftoken=${csrftoken}`);
+  const cookies = cookiesArray.join('; ');
+
+
   const headers = {
     "Accept": "*/*",
     "Content-Type": "application/json",
