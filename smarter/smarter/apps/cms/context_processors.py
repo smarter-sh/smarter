6954eb526447c68a48eac47a9075eaf16b7143d8
lpm0073 <lpm0073@gmail.com>
2025-01-07 10:51:03 -0600

    fix: change logo anchor to '/' for authenticated users

--- smarter/smarter/apps/cms/context_processors.py
+++ smarter/smarter/apps/cms/context_processors.py
@@ -12,9 +12,14 @@ logger = logging.getLogger(__name__)
 # pylint: disable=W0613
 def base(request):
     """Base context processor for templates that inherit from docs/base.html"""
+    if request.user.is_authenticated:
+        smarter_home_url = "/"
+    else:
+        smarter_home_url = smarter_settings.marketing_site_url
     docs_context = {
         "smarter_version": "v" + __version__,
         "smarter_marketing_site_url": smarter_settings.marketing_site_url,
+        "smarter_home_url": smarter_home_url,
         "smarter_logo": smarter_settings.logo,
     }
     return docs_context
