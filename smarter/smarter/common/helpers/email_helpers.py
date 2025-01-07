1c543aba33f7e80dca2d1334c90187acc0dc479d
lpm0073 <lpm0073@gmail.com>
2025-01-07 13:32:56 -0600

    fix: sidebar menu active on clicked item

--- smarter/smarter/common/helpers/email_helpers.py
+++ smarter/smarter/common/helpers/email_helpers.py
@@ -27,7 +27,11 @@ class EmailHelper(metaclass=Singleton):
         valid_emails = [email for email in mailto_list if SmarterValidator.is_valid_email(email)]
 
         if len(valid_emails) != len(mailto_list) and set(mailto_list) != set(valid_emails) and not quiet:
-            logger.warning("invalid email addresses were found in send list: %s", set(mailto_list) - set(valid_emails))
+            diff = set(mailto_list) != set(valid_emails)
+            if diff != [""]:
+                logger.warning(
+                    "invalid email addresses were found in send list: %s", set(mailto_list) - set(valid_emails)
+                )
 
         if len(valid_emails) == 0 and not quiet:
             logger.warning("no valid email addresses found in send list")
