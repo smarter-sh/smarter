25b48a48b78878a9ce79c8c0a0685d7884c5bfb8
lpm0073 <lpm0073@gmail.com>
2025-01-08 10:06:48 -0600

    chore: lint

--- smarter/smarter/__version__.py
+++ smarter/smarter/__version__.py
@@ -1,9 +1,3 @@
 # DO NOT EDIT.
 # Managed via automated CI/CD in .github/workflows/semanticVersionBump.yml.
-__version__ = "0.7.3"
-# DO NOT EDIT.
-# Managed via automated CI/CD in .github/workflows/semanticVersionBump.yml.
-__version__ = "0.7.4"
-# DO NOT EDIT.
-# Managed via automated CI/CD in .github/workflows/semanticVersionBump.yml.
 __version__ = "0.7.5"
