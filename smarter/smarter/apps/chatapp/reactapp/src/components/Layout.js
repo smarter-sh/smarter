f45e6c437336891e477a74c3cffa7e148d37c522
lpm0073 <lpm0073@gmail.com>
2025-01-10 15:43:28 -0600

    feat: scaffold new console output window pane

--- smarter/smarter/apps/chatapp/reactapp/src/components/Layout.js
+++ smarter/smarter/apps/chatapp/reactapp/src/components/Layout.js
@@ -11,14 +11,19 @@ export const ContentLayout = styled.div`
   flex: 1;
   display: flex;
   flex-direction: row;
+  margin: 0;
+  padding: 0;
 `;
 
 export const ChatAppWrapper = styled.div`
   flex-basis: 33.33%;
+  margin: 0;
+  padding: 5px;
 `;
 
 export const ConsoleOutputWrapper = styled.div`
   flex-basis: 66.67%;
-  padding: 10px; /* Add padding to ensure content is not pushed against the right side */
+  padding: 5px;
+  margin: 0;
   box-sizing: border-box; /* Ensure padding is included in the element's total width and height */
 `;
