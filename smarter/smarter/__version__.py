484371aa8007040f45bc4a099ce1f0724e8eae81
lpm0073 <lpm0073@gmail.com>
2025-01-22 15:33:48 -0600

    style: cleanup docs

--- smarter/smarter/__version__.py
+++ smarter/smarter/__version__.py
@@ -1,9 +1,3 @@
 # DO NOT EDIT.
 # Managed via automated CI/CD in .github/workflows/semanticVersionBump.yml.
-__version__ = "0.10.0"
-# DO NOT EDIT.
-# Managed via automated CI/CD in .github/workflows/semanticVersionBump.yml.
-__version__ = "0.10.1"
-# DO NOT EDIT.
-# Managed via automated CI/CD in .github/workflows/semanticVersionBump.yml.
 __version__ = "0.10.2"
