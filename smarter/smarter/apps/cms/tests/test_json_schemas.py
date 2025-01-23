5ac0d9efff9a3b7c4acdca61f66e30ec73ca03d5
lpm0073 <lpm0073@gmail.com>
2025-01-23 09:27:18 -0600

    style: fix mispelled function names

--- smarter/smarter/apps/cms/tests/test_json_schemas.py
+++ smarter/smarter/apps/cms/tests/test_json_schemas.py
@@ -33,7 +33,7 @@ class TestApiDocsJsonSchemas(unittest.TestCase):
         """Tear down test fixtures."""
         admin_user_teardown(cls.user, cls.account, cls.user_profile)
 
-    def test_get_unauthenitcated_json_schemas(self):
+    def test_get_unauthenticated_json_schemas(self):
         """
         Test all docs//json-schema/ endpoints with an unauthenticated user
         to ensure that we get a 200 response
