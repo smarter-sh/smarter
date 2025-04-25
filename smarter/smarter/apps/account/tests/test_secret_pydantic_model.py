475696e1ecf3ea01f73159869818a8e5ddefd243
lpm0073 <lpm0073@gmail.com>
2025-04-25 08:39:14 -0600

    test: write more unit tests for Secret

--- smarter/smarter/apps/account/tests/test_secret_pydantic_model.py
+++ smarter/smarter/apps/account/tests/test_secret_pydantic_model.py
@@ -12,7 +12,7 @@ from smarter.apps.account.manifest.models.secret.model import SAMSecret
 from smarter.apps.account.models import Account, UserProfile
 from smarter.common.utils import dict_is_contained_in
 from smarter.lib.django.user import User
-from smarter.lib.manifest.loader import SAMLoader
+from smarter.lib.manifest.loader import SAMLoader, SAMLoaderError
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -72,6 +72,9 @@ class TestSmarterSecretPydanticModel(unittest.TestCase):
             pass
 
     def test_manifest_initalization_good(self):
+        """
+        Test the manifest initialization with a good manifest file.
+        """
 
         filespec = self.get_data_full_filepath("secret-good.yaml")
         loader = SAMLoader(file_path=filespec)
@@ -85,6 +88,10 @@ class TestSmarterSecretPydanticModel(unittest.TestCase):
         # self.assertTrue(dict_is_contained_in(content, round_trip_dict))
 
     def test_manifest_initalization_bad(self):
+        """
+        Test the manifest initialization with a manifest file
+        this is missing the required spec key, 'value'.
+        """
 
         filespec = self.get_data_full_filepath("secret-bad.yaml")
         loader = SAMLoader(file_path=filespec)
@@ -95,3 +102,32 @@ class TestSmarterSecretPydanticModel(unittest.TestCase):
         self.assertIn("1 validation error for SAMSecret", str(context.exception))
         self.assertIn("spec.config.value", str(context.exception))
         self.assertIn("Field required [type=missing", str(context.exception))
+
+    def test_manifest_initalization_bad2(self):
+        """
+        Test the manifest initialization with a manifest file
+        this is missing the required metadata key, 'description'.
+        """
+
+        filespec = self.get_data_full_filepath("secret-bad2.yaml")
+        with self.assertRaises(SAMLoaderError) as context:
+            SAMLoader(file_path=filespec)
+
+        self.assertIn("Missing required key description", str(context.exception))
+
+    def test_manifest_initalization_bad3(self):
+        """
+        Test the manifest initialization with a manifest file
+        that has an invalid expiration_date
+        """
+
+        filespec = self.get_data_full_filepath("secret-bad3.yaml")
+        loader = SAMLoader(file_path=filespec)
+
+        with self.assertRaises(ValidationError) as context:
+            SAMSecret(**loader.pydantic_model_dump())
+
+        # Assert that the exception message contains the expected details
+        self.assertIn("1 validation error for SAMSecret", str(context.exception))
+        self.assertIn("spec.config.expiration_date", str(context.exception))
+        self.assertIn("Input should be a valid date or datetime", str(context.exception))
