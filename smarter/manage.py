c0104e8ce7689d515e396c3a07614c67dc5e1118
lpm0073 <lpm0073@gmail.com>
2025-05-05 15:34:07 -0600

    chore: pimp manage.py with more bells and whistles

--- smarter/manage.py
+++ smarter/manage.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 # pylint: disable=C0415
 """Django's command-line utility for administrative tasks."""
+import logging
 import os
 import sys
 
 from smarter.common.conf import settings as smarter_settings
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 def main():
     """Run administrative tasks."""
+    if not smarter_settings.environment:
+        raise RuntimeError("The 'smarter_settings.environment' variable is not set.")
+    logger.info("Environment variable 'smarter_settings.environment': %s", smarter_settings.environment)
+
     os.environ["DJANGO_SETTINGS_MODULE"] = "smarter.settings." + smarter_settings.environment
+    logger.info("Using Django settings module: %s", os.environ["DJANGO_SETTINGS_MODULE"])
+
+    if os.getenv("DEBUG", "false").lower() == "true":
+        logging.getLogger().setLevel(logging.DEBUG)
+        logger.debug("Debug mode is enabled.")
 
     try:
         from django.core.management import execute_from_command_line
