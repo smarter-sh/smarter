0beb107040e6ed76ffcbbe8009d34e18ed11b0e5
lpm0073 <lpm0073@gmail.com>
2025-05-16 09:02:14 -0600

    fix: index created_at and updated_at

--- smarter/smarter/lib/django/model_helpers.py
+++ smarter/smarter/lib/django/model_helpers.py
@@ -6,8 +6,8 @@ from django.db import models
 class TimestampedModel(models.Model):
     """Timestamped model to use as a baseclass for all models in this project."""
 
-    created_at = models.DateTimeField(auto_now_add=True, null=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, null=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, editable=False, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, editable=False, db_index=True)
 
     # pylint: disable=missing-class-docstring
     class Meta:
@@ -16,3 +16,11 @@ class TimestampedModel(models.Model):
     def validate(self):
         """Validate the model."""
         self.full_clean()
+
+    def save(self, *args, **kwargs):
+        """Override save to validate before saving."""
+        self.validate()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={getattr(self, 'id', None)})"
