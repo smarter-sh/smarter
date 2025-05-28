c525cefe80eede8ca04627992791846030e23b9a
lpm0073 <lpm0073@gmail.com>
2025-05-28 16:35:54 -0600

    refactor: remove namespace from cms

--- smarter/smarter/apps/cms/const.py
+++ smarter/smarter/apps/cms/const.py
@@ -3,4 +3,3 @@
 CMS_DUMP_PATH = "smarter/apps/cms/data/"
 
 WAGTAIL_DUMP = CMS_DUMP_PATH + "wagtail-data.json"
-namespace = "cms"
--- smarter/smarter/apps/cms/urls.py
+++ smarter/smarter/apps/cms/urls.py
@@ -3,10 +3,6 @@
 from django.urls import include, path
 from wagtail.admin import urls as wagtailadmin_urls
 
-from .const import namespace
-
-
-app_name = namespace
 
 urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
