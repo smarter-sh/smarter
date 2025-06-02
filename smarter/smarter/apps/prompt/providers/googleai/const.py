1cb44c8dd03ce02e811bc53a82e928d15ce4fb3c
lpm0073 <lpm0073@gmail.com>
2025-06-02 12:02:59 -0600

    chore: bump to 2.0 model family

--- smarter/smarter/apps/prompt/providers/googleai/const.py
+++ smarter/smarter/apps/prompt/providers/googleai/const.py
@@ -4,15 +4,14 @@ This module contains constants for the GoogleAI Gemini provider.
 
 BASE_URL = "https://generativelanguage.googleapis.com/v1beta/openai/"  # don't forget the trailing slash
 PROVIDER_NAME = "googleai"
-DEFAULT_MODEL = "gemini-1.5-flash"
+DEFAULT_MODEL = "gemini-2.0-flash"
 
 
 # https://ai.google.dev/gemini-api/docs/models/gemini
 VALID_CHAT_COMPLETION_MODELS = [
     "gemini-2.0-flash-exp",
-    "gemini-1.5-flash",
-    "gemini-1.5-flash-8b",
-    "gemini-1.5-pro",
-    "gemini-1.0-pro",
+    "gemini-2.0-flash",
+    "gemini-2.0-flash-8b",
+    "gemini-2.0-pro",
     "aqa",
 ]
