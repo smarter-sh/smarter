c4bb7232ef653d541509ee749d50fc78cd6aca2e
lpm0073 <lpm0073@gmail.com>
2025-10-13 17:17:55 -0600

    fix: setup api domain and api receivers

--- smarter/smarter/hosts.py
+++ smarter/smarter/hosts.py
@@ -11,27 +11,29 @@ from smarter.common.const import (
 )
 
 
+SMARTER_URLS_MODULE = "smarter.urls"
+SMARTER_URLS_API_MODULE = "smarter.urls_api"
 host_patterns = patterns(
     "",
     host(
-        rf"{SMARTER_PLATFORM_SUBDOMAIN}", "smarter.urls", name=SMARTER_PLATFORM_SUBDOMAIN
+        rf"{SMARTER_PLATFORM_SUBDOMAIN}", SMARTER_URLS_MODULE, name=SMARTER_PLATFORM_SUBDOMAIN
     ),  # for https://platform.smarter.sh/
     host(
-        rf"{SMARTER_API_SUBDOMAIN}", "smarter.urls_api", name=SMARTER_API_SUBDOMAIN
+        rf"{SMARTER_API_SUBDOMAIN}", SMARTER_URLS_API_MODULE, name=SMARTER_API_SUBDOMAIN
     ),  # for https://api.platform.smarter.sh/
     host(
         rf"{SmarterEnvironments.ALPHA}.{SMARTER_API_SUBDOMAIN}",
-        "smarter.urls_api",
+        SMARTER_URLS_API_MODULE,
         name=f"{SmarterEnvironments.ALPHA}_{SMARTER_API_SUBDOMAIN}",
     ),  # for https://alpha.api.platform.smarter.sh/
     host(
         rf"{SmarterEnvironments.BETA}.{SMARTER_API_SUBDOMAIN}",
-        "smarter.urls_api",
+        SMARTER_URLS_API_MODULE,
         name=f"{SmarterEnvironments.BETA}_{SMARTER_API_SUBDOMAIN}",
     ),  # for https://beta.api.platform.smarter.sh/
     host(
         rf"{SmarterEnvironments.NEXT}.{SMARTER_API_SUBDOMAIN}",
-        "smarter.urls_api",
+        SMARTER_URLS_API_MODULE,
         name=f"{SmarterEnvironments.NEXT}_{SMARTER_API_SUBDOMAIN}",
     ),  # for https://next.api.platform.smarter.sh/
 )
