d49fcc3531245d5b1073f41f78df11f5c94e995f
lpm0073 <lpm0073@gmail.com>
2025-10-13 17:10:35 -0600

    fix: setup api domain and api receivers

--- smarter/smarter/hosts.py
+++ smarter/smarter/hosts.py
@@ -4,7 +4,11 @@ Django Hosts configuration.
 
 from django_hosts import host, patterns
 
-from smarter.common.const import SMARTER_API_SUBDOMAIN, SMARTER_PLATFORM_SUBDOMAIN
+from smarter.common.const import (
+    SMARTER_API_SUBDOMAIN,
+    SMARTER_PLATFORM_SUBDOMAIN,
+    SmarterEnvironments,
+)
 
 
 host_patterns = patterns(
@@ -15,4 +19,13 @@ host_patterns = patterns(
     host(
         rf"{SMARTER_API_SUBDOMAIN}", "smarter.urls_api", name=SMARTER_API_SUBDOMAIN
     ),  # for https://api.platform.smarter.sh/
+    host(
+        rf"{SmarterEnvironments.ALPHA}.{SMARTER_API_SUBDOMAIN}", "smarter.urls_api", name=SMARTER_API_SUBDOMAIN
+    ),  # for https://alpha.api.platform.smarter.sh/
+    host(
+        rf"{SmarterEnvironments.BETA}.{SMARTER_API_SUBDOMAIN}", "smarter.urls_api", name=SMARTER_API_SUBDOMAIN
+    ),  # for https://beta.api.platform.smarter.sh/
+    host(
+        rf"{SmarterEnvironments.NEXT}.{SMARTER_API_SUBDOMAIN}", "smarter.urls_api", name=SMARTER_API_SUBDOMAIN
+    ),  # for https://next.api.platform.smarter.sh/
 )
