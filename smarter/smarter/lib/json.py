95cffb8b41baf7b4b9f1a0d42a3a3c0bb4422a73
lpm0073 <lpm0073@gmail.com>
2025-11-29 16:03:11 -0600

    fix: remove import django.core.serializers.json.DjangoJSONEncoder

--- smarter/smarter/lib/json.py
+++ smarter/smarter/lib/json.py
@@ -5,7 +5,10 @@ Overridden JSON utilities. The effective modifications are
 - Use str as the default for non-serializable objects
 """
 
+import datetime
+import decimal
 import json
+import uuid
 
 # pylint: disable=unused-import
 from json import (  # unmodified re-export
@@ -16,7 +19,85 @@ from json import (  # unmodified re-export
     loads,
 )
 
-from django.core.serializers.json import DjangoJSONEncoder
+
+class Promise:
+    """
+    Base class for the proxy class created in the closure of the lazy function.
+    It's used to recognize promises in code.
+    """
+
+    pass
+
+
+def _get_duration_components(duration):
+    days = duration.days
+    seconds = duration.seconds
+    microseconds = duration.microseconds
+
+    minutes = seconds // 60
+    seconds %= 60
+
+    hours = minutes // 60
+    minutes %= 60
+
+    return days, hours, minutes, seconds, microseconds
+
+
+def is_aware(value):
+    """
+    Determine if a given datetime.datetime is aware.
+
+    The concept is defined in Python's docs:
+    https://docs.python.org/library/datetime.html#datetime.tzinfo
+
+    Assuming value.tzinfo is either None or a proper datetime.tzinfo,
+    value.utcoffset() implements the appropriate logic.
+    """
+    return value.utcoffset() is not None
+
+
+def duration_iso_string(duration):
+    if duration < datetime.timedelta(0):
+        sign = "-"
+        duration *= -1
+    else:
+        sign = ""
+
+    days, hours, minutes, seconds, microseconds = _get_duration_components(duration)
+    ms = ".{:06d}".format(microseconds) if microseconds else ""
+    return "{}P{}DT{:02d}H{:02d}M{:02d}{}S".format(sign, days, hours, minutes, seconds, ms)
+
+
+class DjangoJSONEncoder(json.JSONEncoder):
+    """
+    JSONEncoder subclass that knows how to encode date/time, decimal types, and
+    UUIDs.
+    """
+
+    def default(self, o):
+        # See "Date Time String Format" in the ECMA-262 specification.
+        if isinstance(o, datetime.datetime):
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:23] + r[26:]
+            if r.endswith("+00:00"):
+                r = r.removesuffix("+00:00") + "Z"
+            return r
+        elif isinstance(o, datetime.date):
+            return o.isoformat()
+        elif isinstance(o, datetime.time):
+            if is_aware(o):
+                raise ValueError("JSON can't represent timezone-aware times.")
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:12]
+            return r
+        elif isinstance(o, datetime.timedelta):
+            return duration_iso_string(o)
+        elif isinstance(o, (decimal.Decimal, uuid.UUID, Promise)):
+            return str(o)
+        else:
+            return super().default(o)
 
 
 def dumps(
