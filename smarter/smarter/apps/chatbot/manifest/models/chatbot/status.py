1b76d68a1626d987ab5043c5f815b577aa207574
lpm0073 <lpm0073@gmail.com>
2026-03-14 12:03:20 -0600

    fix: refactor chatbot broker to use SAM model for rendering json

--- smarter/smarter/apps/chatbot/manifest/models/chatbot/status.py
+++ smarter/smarter/apps/chatbot/manifest/models/chatbot/status.py
@@ -2,7 +2,7 @@
 
 import os
 from datetime import datetime
-from typing import ClassVar
+from typing import ClassVar, Optional
 
 from pydantic import Field
 
@@ -18,22 +18,54 @@ class SAMChatbotStatus(AbstractSAMStatusBase):
 
     class_identifier: ClassVar[str] = MODULE_IDENTIFIER
 
-    account_number: str = Field(
-        None,
+    accountNumber: str = Field(
         description=f"{class_identifier}.account_number: The account owner of this {MANIFEST_KIND}. Read only.",
     )
 
     username: str = Field(
-        None,
-        description=f"{class_identifier}.account_number: The Smarter user who created this {MANIFEST_KIND}. Read only.",
+        description=f"{class_identifier}.username: The Smarter user who created this {MANIFEST_KIND}. Read only.",
     )
 
     created: datetime = Field(
-        None,
         description=f"{class_identifier}.created: The date in which this {MANIFEST_KIND} was created. Read only.",
     )
 
     modified: datetime = Field(
-        None,
         description=f"{class_identifier}.modified: The date in which this {MANIFEST_KIND} was most recently changed. Read only.",
+    )
+    deployed: bool = Field(
+        description=f"{class_identifier}.deployed: Whether this {MANIFEST_KIND} is currently deployed. Read only.",
+    )
+    defaultHost: str = Field(
+        description=f"{class_identifier}.defaultHost: The default host URL for this {MANIFEST_KIND}. Read only.",
+    )
+    defaultUrl: str = Field(
+        description=f"{class_identifier}.defaultUrl: The default URL for this {MANIFEST_KIND}. Read only.",
+    )
+    customUrl: Optional[str] = Field(
+        description=f"{class_identifier}.customUrl: The custom URL for this {MANIFEST_KIND}. Read only.",
+    )
+    sandboxHost: str = Field(
+        description=f"{class_identifier}.sandboxHost: The sandbox host URL for this {MANIFEST_KIND}. Read only.",
+    )
+    sandboxUrl: str = Field(
+        description=f"{class_identifier}.sandboxUrl: The sandbox URL for this {MANIFEST_KIND}. Read only.",
+    )
+    hostname: str = Field(
+        description=f"{class_identifier}.hostname: The hostname for this {MANIFEST_KIND}. Read only.",
+    )
+    url: str = Field(
+        description=f"{class_identifier}.url: The URL for this {MANIFEST_KIND}. Read only.",
+    )
+    urlChatbot: str = Field(
+        description=f"{class_identifier}.urlChatbot: The chatbot URL for this {MANIFEST_KIND}. Read only.",
+    )
+    urlChatapp: str = Field(
+        description=f"{class_identifier}.urlChatapp: The chatapp URL for this {MANIFEST_KIND}. Read only.",
+    )
+    urlChatConfig: str = Field(
+        description=f"{class_identifier}.urlChatConfig: The chat config URL for this {MANIFEST_KIND}. Read only.",
+    )
+    dnsVerificationStatus: str = Field(
+        description=f"{class_identifier}.dnsVerificationStatus: The DNS verification status for this {MANIFEST_KIND}. Read only.",
     )
