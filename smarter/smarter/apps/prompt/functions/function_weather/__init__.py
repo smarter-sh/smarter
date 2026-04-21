abe73c22ed45b34684d2eaa5f710ef41ce724403
lpm0073 <lpm0073@gmail.com>
2026-04-21 17:52:31 -0600

    refactor: move all modules into a separate folder

--- smarter/smarter/apps/prompt/functions/function_weather/__init__.py
+++ smarter/smarter/apps/prompt/functions/function_weather/__init__.py
@@ -2,7 +2,7 @@
 Get the current weather in a given location and format it as a string.
 """
 
-from .function_weather import get_current_weather, weather_tool_factory
+from .protocol import get_current_weather, weather_tool_factory
 
 __all__ = [
     "get_current_weather",
