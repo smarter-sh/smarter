dbd4237e298c12be84c401e7bbc726c7dfe2a01e
lpm0073 <lpm0073@gmail.com>
2026-04-26 13:11:05 -0600

    docs: add inline documentation

--- smarter/react/prompt_passthrough/vite.config.ts
+++ smarter/react/prompt_passthrough/vite.config.ts
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'development' ? '/' : '/static/terminal_emulator/',
+  // runtime builds are saved into the Django static directory so that these
+  // files can be included in the Django collectstatic process and served by
+  // Django at runtime. On the other hand, in development we want to rely on
+  // Vite's dev server to serve these files, so we set the base to '/'.
+  base: process.env.NODE_ENV === 'development' ? '/' : '/static/prompt_passthrough/',
   build: {
     outDir: "../../smarter/static/prompt_passthrough",
     emptyOutDir: true,
@@ -15,6 +19,16 @@ export default defineConfig({
       },
     },
   },
+  // Django collects static files and serves them from /static/
+  // We need to create proxy servers in React's dev environment
+  // so that these requests are served from the Django dev server instead
+  // of the React dev server.
+  //
+  // Most of these cases stem from <link> elements added to this index.html
+  // containing platform-wide stylesheets and scripts that originate from
+  // and are served by the Django dev server. These are added to index.html
+  // in order to keep this React dev environment as close to the runtime
+  // environment as possible.
   server: {
     proxy: {
       '/api': 'http://localhost:9357',
