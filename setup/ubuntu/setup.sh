b7452952a72f5a847ce302a6ad80ec4f5827d0ce
lpm0073 <lpm0073@gmail.com>
2026-05-27 15:25:28 -0600

    chore: install all dependencies

--- setup/ubuntu/setup.sh
+++ setup/ubuntu/setup.sh
@@ -43,6 +43,17 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew install python@3.13
 
+alias python="/home/linuxbrew/.linuxbrew/opt/python@3.13/libexec/bin/python"
+alias python3="/home/linuxbrew/.linuxbrew/opt/python@3.13/libexec/bin/python3"
+
+# Prepend the Homebrew Python bin directory to PATH
+export PATH="/home/linuxbrew/.linuxbrew/opt/python@3.13/libexec/bin:$PATH"
+
+# Optionally, set these for building Python packages (Linuxbrew path)
+export LDFLAGS="-L/home/linuxbrew/.linuxbrew/opt/python@3.13/lib"
+export CPPFLAGS="-I/home/linuxbrew/.linuxbrew/opt/python@3.13/include"
+export PKG_CONFIG_PATH="/home/linuxbrew/.linuxbrew/opt/python@3.13/lib/pkgconfig"
+
 sudo snap install k9s
 
 # NVM (Node Version Manager) is not available via apt. Install manually:
