f63fb2cc20817d81cd5d0295158a191aa6ddab81
lpm0073 <lpm0073@gmail.com>
2026-06-25 07:34:51 -0600

    refactor: port django template html and styling. port file open dialogue

--- smarter/react/packages/smarter-drop-zone/src/components/DropZone/Component.tsx
+++ smarter/react/packages/smarter-drop-zone/src/components/DropZone/Component.tsx
@@ -18,25 +18,81 @@
  *       cookieDomain=".smarter.sh"
  *     />
  */
+import { useRef } from "react";
 import type { SessionContext } from "@smarter/common";
 
 import "./styles.css";
 
-
 function DropZone({ sessionContext }: { sessionContext: SessionContext }) {
+  console.debug("DropZone: Rendering with sessionContext:", sessionContext);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileOpen = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileSelected = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (!file) {
+      return;
+    }
+
+    console.debug("Selected file:", file);
+
+    // TODO:
+    // read file
+    // validate extension
+    // parse YAML/JSON
+    // update component state
+  };
 
   return (
     <>
-      <section
-        id="kt_app_content"
-        aria-label="DropZone"
-        className="app-content flex-column-fluid"
-      >
-        <div
-          id="kt_app_content_container"
-          className="app-container container-xxl"
-        >
-          <div className="row g-5 g-xl-10 mt-3">hello world sessionContext: {JSON.stringify(sessionContext)}</div>
+      <section id="manifest-apply">
+        <div id="kt_app_content" className="app-content flex-column-fluid">
+          {/* begin::Content container */}
+          <div id="kt_app_content_container" className="app-container container-xxl">
+            {/* begin::Row */}
+            <div className="row g-5 g-xl-10">
+              {/* begin::Col */}
+              <div className="col-xl-8 mb-5 mb-xl-10">
+                {/* begin::Row */}
+                <div className="g-5 g-xl-10 mb-l-10">
+                  <h3 className="pt-5">Apply Smarter YAML Manifest</h3>
+                  {/* begin:: File Open */}
+                  <div className="file-open-command-button mt-5 mb-5">
+                    <div className="col-10"></div>
+                    <div className="col-2">
+                      <input
+                        ref={fileInputRef}
+                        type="file"
+                        accept=".yaml,.yml,.json"
+                        style={{ display: "none" }}
+                        onChange={handleFileSelected}
+                      />
+                      <button type="button" className="btn btn-sm btn-primary" onClick={handleFileOpen}>
+                        File Open
+                      </button>
+                    </div>
+                  </div>
+                  {/* end:: File Open */}
+                  {/* begin:: Drop Zone */}
+                  <div
+                    id="drop-zone-overlay"
+                    className="manifest-drop-zone d-flex justify-content-center align-items-center"
+                  >
+                    <span>Drop Zone</span>
+                  </div>
+                  {/* end:: Drop Zone */}
+                </div>
+                {/* end::Row */}
+              </div>
+              {/* end::Col */}
+            </div>
+            {/* end::Row */}
+          </div>
+          {/* end::Content container */}
         </div>
       </section>
     </>
